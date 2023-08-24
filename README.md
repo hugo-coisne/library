1092fdbab25bcf1d0c5584c85551869445eb85f7
awadesu <133776560+awadesu@users.noreply.github.com>
2023-08-24 18:02:01 +0200

    temporary finish

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-change functions for delete and toggleread to mutate library
-
-make display function work by removing all td from the table before displaying them
+the js code is basically thrash, I will return to it later on
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// le port de la ceinture de sécurité est recommendé pour la lecture de ce code
+
 let library = [];
 
 function Book(title, author, npages, read) {
@@ -22,11 +24,13 @@ function removeAllChildNodes(parent) {
 const table = document.querySelector("tbody");
 
 function displayLibrary() {
-  while (table.children > 1) {
+  while (table.children.length > 1) {
     table.lastChild.remove;
-  }
+  } // remove everything from the current table
+
   for (let i in library) {
     const book = document.createElement("tr");
+
     function datacells() {
       for (let j in library[i]) {
         if (j == "read") {
@@ -47,10 +51,20 @@ function displayLibrary() {
       toggle.onclick = function () {
         let read = Array.from(this.parentNode.parentNode.children)[3]
           .children[0];
-        if (read.innerText === "yes") {
+        let bbook = this.parentNode.parentNode.children[0].innerText;
+        let x = 0;
+        for (let k in library) {
+          if (library[k].title === bbook) {
+            x = k;
+            break;
+          }
+        }
+        if (library[x].read === "yes") {
           read.innerText = "no";
+          library[x].read = "no";
         } else {
           read.innerText = "yes";
+          library[x].read = "yes";
         }
       };
       td2.appendChild(toggle);
@@ -65,9 +79,13 @@ function displayLibrary() {
       deleter.innerText = "Delete";
       deleter.onclick = function () {
         let bbook = this.parentNode.parentNode;
-        
-        console.log(bbook);
-        bbook.remove();
+        for (let k in library) {
+          if (bbook.firstChild.innerHTML === library[k].title) {
+            library.splice(k, 1);
+            bbook.remove();
+            break;
+          }
+        }
       };
 
       td3.appendChild(deleter);
@@ -84,11 +102,71 @@ function submit() {
   const author = document.querySelector("#bauthor");
   const pn = document.querySelector("#bnpages");
   const read = document.querySelector("#bread");
-  let r = "yes";
-  if (!read) {
-    let r = "no";
+  if ((title.value == "") | (author.value == "") | (pn.value == "")) {
+    alert("Please fill all the fields before submitting");
+    return;
   }
+  let r = "yes";
+  if (!read.checked) {
+    r = "no";
+  }
+
   const b = new Book(title.value, author.value, pn.value, r);
+  title.value = "";
+  author.value = "";
+  pn.value = "";
+  read.checked = false;
   library.push(b);
-  displayLibrary();
+  const book = document.createElement("tr");
+  for (let j in library[library.length - 1]) {
+    if (j == "read") {
+      continue;
+    }
+    const td = document.createElement("td");
+    td.innerHTML = library[library.length - 1][j];
+    book.appendChild(td);
+  }
+  const toggle = document.createElement("button");
+  const td2 = document.createElement("td");
+  td2.classList = "but toggle";
+  toggle.innerHTML = library[library.length - 1].read;
+  toggle.type = "button";
+  toggle.onclick = function () {
+    let read = Array.from(this.parentNode.parentNode.children)[3].children[0];
+    let bbook = this.parentNode.parentNode.children[0].innerText;
+    let x = 0;
+    for (let k in library) {
+      if (library[k].title === bbook) {
+        x = k;
+        break;
+      }
+    }
+    if (library[x].read === "yes") {
+      read.innerText = "no";
+      library[x].read = "no";
+    } else {
+      read.innerText = "yes";
+      library[x].read = "yes";
+    }
+  };
+  td2.appendChild(toggle);
+  book.appendChild(td2);
+  const deleter = document.createElement("button");
+  const td3 = document.createElement("td");
+  td3.classList = "but del";
+  deleter.innerText = "Delete";
+  deleter.onclick = function () {
+    let bbook = this.parentNode.parentNode;
+    for (let k in library) {
+      if (bbook.firstChild.innerHTML === library[k].title) {
+        library.splice(k, 1);
+        bbook.remove();
+        break;
+      }
+    }
+  };
+
+  td3.appendChild(deleter);
+  book.appendChild(td3);
+  table.appendChild(book);
 }
